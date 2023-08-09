67d00fb4eef714a589ebac30fc274164b83fd2df
qutti96 <qutti96@gmail.com>
2023-08-09 12:37:51 +0900

    Feature/func b (#2)
    * added func B-1
    
    * added func B-2

--- pages/index.js
+++ pages/index.js
@@ -13,7 +13,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          FuncA-2 Welcome to <a href="https://nextjs.org">Next.js!</a>
+          FuncB-2 Welcome to <a href="https://nextjs.org">Next.js!</a>
         </h1>
 
         <p className={styles.description}>
